4926398c5768f37febd50b94afa2d0a08d29eb3b
Alexander Shereshevsky <shereshevsky@gmail.com>
2020-09-15 20:57:46 +0300

    test

--- src/settings.py
+++ src/settings.py
@@ -9,11 +9,11 @@ load_dotenv()
 DEFAULT_NAMESPACE = os.getenv("DEFAULT_NAMESPACE")
 DEFAULT_CHUNK_SIZE = int(os.getenv("DEFAULT_CHUNK_SIZE", 6291456))
 DEFAULT_SENTRY = os.getenv("DEFAULT_SENTRY")
-
+DEFAULT_REGION = os.getenv("AWS_DEFAULT_REGION", "eu-central-1")
 AWS_CONFIG = None
 
 if not os.getenv("AWS_ACCESS_KEY_ID"):
-    session = boto3.Session(os.getenv("AWS_DEFAULT_REGION"))
+    session = boto3.Session(DEFAULT_REGION)
     credentials = session.get_credentials()
     credentials = credentials.get_frozen_credentials()
     AWS_CONFIG = credentials
